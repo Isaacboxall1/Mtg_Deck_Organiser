fe6067ab1425deaa8bebd80acd898d27a2192920
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-07-01 21:08:54 +0100

    collection display page added

---
+++ pages/collectiondisplay.js
@@ -0,0 +1,9 @@
+export default function CollectionDisplay () {
+
+    return ( 
+        <div>
+        <h1>Collection Display</h1>
+        <p>Collection display will go here</p>
+        </div>
+    )
+}
