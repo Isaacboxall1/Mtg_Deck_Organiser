75e191b79459f8a597977cb94815422640795a7c
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-07-04 01:01:33 +0100

    started adding functionality to take in and format user Input

--- pages/addcardstocollection.js
+++ pages/addcardstocollection.js
@@ -3,9 +3,24 @@ import { useState } from 'react';
 export default function AddCardsToCollection() {
 
     const [cardsToAddArray, setCardsToAddArray] = useState([]);
+    const [cardsToAdd, setCardsToAdd] = useState('');
+
+    function handleAddCards() {
+        const cardsToAddArray = cardsToAdd.split('\n').filter(card => card !== '');
+        setCardsToAddArray(cardsToAddArray);
+        console.log(cardsToAddArray);
+    }
+
+    function handleInputChange(event) {
+        setCardsToAdd(event.target.value);
+    }
 
     return (<div >
         <h1>Add Cards To Collection</h1>
+        <form onSubmit={(e)=> e.preventDefault()}>
+        <textarea onChange={(e)=> handleInputChange(e)}placeholder='2x Rune Crab' cols={100} style={{resize:'none', minHeight: '200px', maxHeight: "100%"}} ></textarea>
+        <button type='submit' onClick={handleAddCards}>Add Cards to Collection</button>
+        </form>
         </div>)
         
 }
