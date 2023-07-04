4a9c64546d0f68a1e4f9672a825305889283942b
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-07-04 13:26:34 +0100

    user can delete cards from their collection by setting quantity to 0

--- utility/database/changequantity.js
+++ utility/database/changequantity.js
@@ -1,6 +1,6 @@
 import { supabase } from "../config/supabase";
 export async function changeQuantity(card, quantity, userId) {
-    
+    if (quantity > 0) {
     const { data, error } = await supabase
     .from('user_cards')
     .update({ quantity: quantity })
@@ -9,6 +9,16 @@ export async function changeQuantity(card, quantity, userId) {
     console.log(data)
     if (error)
     console.log(`could not update quantity because of ${error}`)
+    }
+    else {
+        const { data, error } = await supabase
+        .from('user_cards')
+        .delete()
+        .match({ user_id: '3b37684c-a075-4b4b-9fe6-6973a26fe766', card_id: card.id })
+        .single()
+        console.log(data)
+        if (error)
+        console.log(error)
+    }
 
-    return data
 }
