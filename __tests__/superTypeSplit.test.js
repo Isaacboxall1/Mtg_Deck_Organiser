ebb3a3a42f78df4b8aab0475576c156348b4cb7d
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-07-12 03:09:13 +0100

    superTypeSplit basic test written

---
+++ __tests__/superTypeSplit.test.js
@@ -0,0 +1,47 @@
+import { superTypeSplit } from "@/utility/functions/formatToStats";// change this to your actual file name
+
+describe('superTypeSplit', () => {
+    it('should correctly count the number of cards of each supertype', () => {
+        const mockSortedArray = [
+            {
+                name: 'Creature',
+                cards: [
+                    { name: 'Bear Cub', quantity: 2 },
+                    { name: 'Giant Spider', quantity: 1 }
+                ]
+            },
+            {
+                name: 'Instant',
+                cards: [
+                    { name: 'Giant Growth', quantity: 3 }
+                ]
+            },
+            {
+                name: 'Artifact',
+                cards: [
+                    { name: 'Sol Ring', quantity: 1 },
+                    { name: 'Thran Dynamo', quantity: 1 }
+                ]
+            },
+            {
+                name: 'Other',
+                cards: [
+                    { name: 'Unknown Card', quantity: 2 }
+                ]
+            }
+        ];
+
+        const expected = [
+            { name: 'Creature', value: 2 },
+            { name: 'Instant', value: 1 },
+            { name: 'Sorcery', value: 0 },
+            { name: 'Enchantment', value: 0 },
+            { name: 'Artifact', value: 2 },
+            { name: 'Land', value: 0 },
+            { name: 'Planeswalker', value: 0 },
+            { name: 'Other', value: 1 }
+        ];
+
+        expect(superTypeSplit(mockSortedArray)).toEqual(expected);
+    });
+});
