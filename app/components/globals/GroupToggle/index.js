5fefa7111add69b280f9cf5190ab6a9ea5b2739a
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-07-12 11:36:57 +0100

    styled group toggle

--- app/components/globals/GroupToggle/index.js
+++ app/components/globals/GroupToggle/index.js
@@ -2,10 +2,12 @@ import styles from './grouptoggle.module.css';
 export default function GroupToggle({setSortCriteria, sortCriteria}) {
 
     return ( 
+    <div className={styles.groupToggleContainer}>
+        <label>Group By:</label>
     <div className={styles.groupToggle}>
-    <h2>Group By:</h2>
     <button className={sortCriteria === 'type' ? styles.active : 'inactive'} onClick={()=> {setSortCriteria('type')}}>Type</button>
     <button className={sortCriteria === 'color' ? styles.active : 'inactive'} onClick={()=> {setSortCriteria('color')}}>Color</button>
     <button className={sortCriteria === 'rarity' ? styles.active : 'inactive'} onClick={()=> {setSortCriteria('rarity')}}>Rarity</button>
+    </div>
     </div>)
 }
