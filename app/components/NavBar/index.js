4b50d5787e615cd16910f8d6437aaa1edc3a3a89
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-07-16 14:48:09 +0100

    user and session are pulled dynamically from context and displaying on screen

--- app/components/NavBar/index.js
+++ app/components/NavBar/index.js
@@ -1,10 +1,20 @@
+'use client'
 import styles from './navbar.module.css'
 import LogOutButton from '../globals/LogOutButton'
+import { useAuth } from '../ContextProvider'
+import { useEffect } from 'react'
+
 export default function NavBar() {
+
+    const { user } = useAuth()
+    useEffect(() => {   
+        console.log(user)
+    }, [user])
     return (
         <div className={styles.NavBar}>
 
             <h1>NavBar</h1>
+            <p>Welcome back {user? user[0].first_name : null}!</p>
             <LogOutButton />
         </div>
     )
