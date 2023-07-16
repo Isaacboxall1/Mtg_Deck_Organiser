6f69af90efc0313edba1f4fc94b4d6a4b5892976
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-07-16 14:41:21 +0100

    log out button working and logging user out

--- app/components/globals/LogOutButton/index.js
+++ app/components/globals/LogOutButton/index.js
@@ -1,10 +1,24 @@
+'use client'
+
 import styles from './logoutbutton.module.css'
+import { handleLogOut } from '@/utility/database/supabase/handleLogOut'
+import { useAuth } from '../../ContextProvider';
+import { useRouter } from 'next/navigation'
 
 export default function LogOutButton() {
 
+    const router = useRouter()
+
+    const { setUser, setSession } = useAuth()
+
+    async function logOutAndHome () {
+        await handleLogOut(setSession, setUser); 
+        router.push('/')
+        }
+
     return (
         <div className={styles.LogOutButton}>
-            <h1>LogOutButton</h1>
+            <button onClick={logOutAndHome}>Log Out</button>
         </div>
     )
 }
