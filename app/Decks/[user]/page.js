99ea172936a887073e9a37c058f19211d802c317
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-07-31 01:06:48 +0100

    deck preview card tested with multiple deck styles

--- app/Decks/[user]/page.js
+++ app/Decks/[user]/page.js
@@ -1,9 +1,78 @@
 import styles from './alluserdecks.module.css'
 import DeckPreview from '@/app/components/DeckPreview';
 export default function UserDecks({ params }) {
+  
+  const deckDetails = {
+    title: "Gisa Deck",
+    colors: ["B", ],
+    format: "EDH/Commander",
+    date: "1 day ago",
+    cardArt:
+      "https://cards.scryfall.io/art_crop/front/7/7/779e3944-4342-4151-9963-87e8d41fd2ff.jpg?1680135257",
+  };
+
+  const deckDetails2 = {
+    title: "Flicker Fun",
+    colors: ["U", "G", "W"],
+    format: "EDH/Commander",
+    date: "1 day ago",
+    cardArt:
+      "https://cards.scryfall.io/art_crop/front/9/9/998d0cc8-ca2a-41c3-ab65-d05c26ab8278.jpg?1673149399",
+  };
+  const deckDetails3 = {
+    title: "Jenara +1+/+1",
+    colors: ["U", "G", "W"],
+    format: "EDH/Commander",
+    date: "1 day ago",
+    cardArt:
+    "https://cards.scryfall.io/art_crop/front/4/b/4bcf5b5f-b908-4975-91f7-c4c6f819e0a6.jpg?1673485098"
+  };
+  const deckDetails4 = {
+    title: "Calix Aura Rush",
+    colors: ["W", "G"],
+    format: "Standard",
+    date: "1 day ago",
+    cardArt:
+    "https://cards.scryfall.io/art_crop/front/0/a/0a707ab3-b9b5-422a-8b0f-e963d3ad6606.jpg?1684340668"
+  }
+  const deckDetails5 = {
+    title: "Goblins",
+    colors: ["R"],
+    format: "Modern",
+    date: "1 Month ago",
+    cardArt:
+    "https://cards.scryfall.io/art_crop/front/c/d/cd9fec9d-23c8-4d35-97c1-9499527198fb.jpg?1601078209"
+  }
+  const deckDetails6 = {
+    title: "Hollow Vine",
+    colors: ["U", "G", "R", "B"],
+    format: "Vintage",
+    date: "6 days ago",
+    cardArt:
+    "https://cards.scryfall.io/art_crop/front/2/6/26bfde99-7761-48e1-851a-522f888d0f6c.jpg?1547517482"
+  }
+
+
+
+
+
+  const DeckArray = [
+    deckDetails,
+    deckDetails2,
+    deckDetails3,
+    deckDetails4,
+    deckDetails5,
+    deckDetails6,
+  ]
+
   return (
     <div className={styles.PageContainer}>
-      <DeckPreview />
+      <h1 className={styles.PageTitle}>{params.user}'s Decks</h1>
+      <div className={styles.DeckList}>
+        {DeckArray.map((deck, index) => {
+          return <DeckPreview key={index} deckDetails={deck} />;
+        })}
+      </div>
     </div>
   );
 }
--- app/components/DeckPreview/index.js
+++ app/components/DeckPreview/index.js
@@ -1,23 +1,26 @@
-import ManaIcon from '../globals/ManaIcon';
-import styles from './deckpreview.module.css';
-import Image from 'next/image';
-export default function DeckPreview(deckDetails) {
-  return <div className={styles.DeckPreviewContainer}>
-        <h1 className={styles.DeckTitle}>Gisa Deck</h1>
-    <div className={styles.CardArtBox}>
-        <Image src="https://cards.scryfall.io/art_crop/front/7/7/779e3944-4342-4151-9963-87e8d41fd2ff.jpg?1680135257" alt='card' width={320} height={230} />
-    </div>
-    <div className={styles.DeckDetails}>
-    <div className={styles.ColorsContainer}>
-        <ManaIcon color="U" size={20} fill="none"/>
-        <ManaIcon color="B" size={20} fill="none"/>
-        <ManaIcon color="R" size={20} fill="none"/>
-        <ManaIcon color="G" size={20} fill="none"/>
-        <ManaIcon color="W" size={20} fill="none"/>
+import ManaIcon from "../globals/ManaIcon";
+import styles from "./deckpreview.module.css";
+import Image from "next/image";
+
+export default function DeckPreview({deckDetails}) {
+
+  const { title, colors, format, date, cardArt } = deckDetails;
+
+  return (
+    <div className={styles.DeckPreviewContainer}>
+      <h1 className={styles.DeckTitle}>{title}</h1>
+      <div className={styles.CardArtBox}>
+        <Image src={cardArt} alt="card" width={340} height={230} />
+      </div>
+      <div className={styles.DeckDetails}>
+        <div className={styles.ColorsContainer}>
+          {colors.map((color, index) => {
+            return <ManaIcon key={index} color={color} size={20} fill="none" />;
+          })}
         </div>
-        <h3>EDH/Commander</h3>
-        <h5>1 day ago</h5>
-        
+        <h3>{format}</h3>
+        <h5>{date}</h5>
+      </div>
     </div>
-  </div>;
+  );
 }
