3de5581c13d3e31e5d85905b7e2680c178c24af3
Isaacboxall1 <Isaacboxall1@gmail.com>
2023-08-07 15:10:39 +0200

    deck seperated into types

--- app/Decks/[user]/[deckname]/page.js
+++ app/Decks/[user]/[deckname]/page.js
@@ -1,59 +1,77 @@
-'use client'
+"use client";
 
-import DisplayListShort from '@/app/components/ListFormattedByType/DisplayList/DisplayListShort';
-import styles from './deckname.module.css'
-import CardDisplayShort from '@/app/components/globals/CardDisplayShort';
+import DisplayListShort from "@/app/components/ListFormattedByType/DisplayList/DisplayListShort";
+import styles from "./deckname.module.css";
+import CardDisplayShort from "@/app/components/globals/CardDisplayShort";
+import { useEffect, useState } from "react";
+import { formatIntoTypes } from "@/utility/functions/formatIntoTypes";
 export default function UserDeckDisplay({ params }) {
-
   const exampleCard = {
-    card_id: '1234',
-    card_name: 'Charging Badger',
+    card_id: "1234",
+    card_name: "Charging Badger",
     quantity: 2,
-    card_type: 'Creature',
-    mana_cost: '{1}{G}{W}',
+    type: "Creature",
+    mana_cost: "{1}{G}{W}",
     price: 0.25,
-    card_image: '',
+    card_image: "",
     commander: false,
     sideboard: false,
-  }
+  };
   const exampleCard2 = {
-    card_id: '1234',
-    card_name: 'Nissa, Harbinger of Example Cards',
+    card_id: "1234",
+    card_name: "Nissa, Harbinger of Example Cards",
     quantity: 4,
-    card_type: 'Legendary Creature',
-    mana_cost: '{3}{G}{G}',
+    type: "Legendary Planeswalker",
+    mana_cost: "{3}{G}{G}",
     price: 0.25,
-    card_image: '',
+    card_image: "",
     commander: false,
     sideboard: false,
-  }
+  };
   const exampleCard3 = {
-    card_id: '1234',
-    card_name: 'Karn, Overpowered planeswalker',
+    card_id: "1234",
+    card_name: "Karn, Overpowered planeswalker",
     quantity: 4,
-    card_type: 'Legendary Creature',
-    mana_cost: '{3}{G}{G}{3}',
+    type: "Planeswalker",
+    mana_cost: "{3}{G}{G}{3}",
     price: 0.25,
-    card_image: '',
+    card_image: "",
     commander: false,
     sideboard: false,
-  }
+  };
 
-  const exampleDeck = [
+  const [exampleDeck, setExampleDeck] = useState([
     exampleCard,
     exampleCard2,
     exampleCard,
     exampleCard3,
     exampleCard,
-  ]
+  ]);
+  
+  const [formattedDeck, setFormattedDeck] = useState([]);
+
+  useEffect(() => {
+    const formattedDeck = formatIntoTypes(exampleDeck);
+    setFormattedDeck(formattedDeck);
+  }, [exampleDeck]);
 
   return (
     <div className={styles.PageContainer}>
-      <h1> This Deck Displays the deck titled {params.deckname}. Created by the user named {params.user}</h1>
-      <DisplayListShort cardArray={exampleDeck} />
-      {/* {exampleDeck.map((card, index) => {
-        return <CardDisplayShort card={card} key={index}/>
-      })} */}
+      <h1>
+        {" "}
+        This Deck Displays the deck titled {params.deckname}. Created by the
+        user named {params.user}
+      </h1>
+      <div>
+        {formattedDeck.map((type, index) => {
+          return (
+            <div key={index}>
+              <h2>{type.name}</h2>
+              <DisplayListShort cardArray={type.cards} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
